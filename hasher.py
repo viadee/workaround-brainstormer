67bd5db38b2899a7661d10a90998b92995fe81c1
SvenHue-dev <sven.huening@viadee.de>
2025-04-24 13:23:38 +0200

    remove hasher

--- hasher.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.security import generate_password_hash
-
-# Generate the hashed password
-password = "mysecurepw"
-hashed_password = generate_password_hash(password)
-
-print("Hashed password:", hashed_password)
