b562a899e41b3a2c0cb6c0e05d61b675d78165e0
Marten Jostmann <marten.jostmann@viadee.de>
2025-04-29 08:36:05 +0200

    feat: update secrets and env variables + tls certificate for dev and prod

--- pulumi/deployments/prod/main.py
+++ pulumi/deployments/prod/main.py
@@ -9,17 +9,21 @@ from key_vaults.secret import create_secret
 from managed_identity.identity import Identity
 import pulumi_azure_native as azure_native
 
+HOSTNAME = "brainstormer.cwa.viadee.cloud"
+
 
 class ProdDeployment:
     def __init__(
         self,
         project_name: str,
         rg_name: str,
+        subscription_id: str,
         location: str,
         opts: pulumi.ResourceOptions,
     ) -> None:
         self.project_name = project_name
         self.rg_name = rg_name
+        self.subscription_id = subscription_id
         self.location = location
         self.opts = opts
 
@@ -28,13 +32,20 @@ class ProdDeployment:
     def build(self) -> None:
         config = pulumi.Config()
 
-        openai_key = config.require("AZURE-API-KEY")
+        openai_key = config.require_secret("AZURE-API-KEY")
         openai_url = config.require("AZURE-API-URL")
-        app_secret_key = config.require("APP-SECRET-KEY")
-        wa_password_hash = config.require("WA-PASSWORD-HASH")
+        openai_version = config.get("AZURE-API-VERSION")
+        app_secret_key = config.require_secret("APP-SECRET-KEY")
+        wa_password_hash = config.require_secret("WA-PASSWORD-HASH")
         wa_username = config.require("WA-USERNAME")
-        admin_password_hash = config.require("ADMIN-PASSWORD-HASH")
+        admin_password_hash = config.require_secret("ADMIN-PASSWORD-HASH")
         daily_cost_threshold = config.require("DAILY-COST-THRESHOLD")
+
+        qdrant_url = config.require("QDRANT-URL")
+        qdrant_workarounds_read_key = config.require_secret(
+            "QDRANT-WORKAROUNDS-READ-KEY"
+        )
+        qdrant_full_access_key = config.require_secret("QDRANT-FULL-ACCESS-KEY")
 
         ApplicationRegistration(
             name=f"{self.project_name}-{self.environment}",
@@ -134,6 +145,26 @@ class ProdDeployment:
             opts=self.opts,
         )
 
+        qdrant_workarounds_read_key_name = "qdrant-workarounds-read-key"
+        qdrant_workarounds_read_key_secret = create_secret(
+            resource_group_name=self.rg_name,
+            secret_name=qdrant_workarounds_read_key_name,
+            key=qdrant_workarounds_read_key,
+            environment=self.environment,
+            keyvault=keyvault,
+            opts=self.opts,
+        )
+
+        qdrant_full_access_key_name = "qdrant-full-access-key"
+        qdrant_full_access_key_secret = create_secret(
+            resource_group_name=self.rg_name,
+            secret_name=qdrant_full_access_key_name,
+            key=qdrant_full_access_key,
+            environment=self.environment,
+            keyvault=keyvault,
+            opts=self.opts,
+        )
+
         secrets = [
             azure_native.app.v20230501.SecretArgs(
                 name=openai_key_secret_name,
@@ -155,29 +186,54 @@ class ProdDeployment:
                 key_vault_url=admin_password_hash_secret.properties.secret_uri,
                 identity=managed_identity.identity.id,
             ),
+            azure_native.app.v20230501.SecretArgs(
+                name=qdrant_workarounds_read_key_name,
+                key_vault_url=qdrant_workarounds_read_key_secret.properties.secret_uri,
+                identity=managed_identity.identity.id,
+            ),
+            azure_native.app.v20230501.SecretArgs(
+                name=qdrant_full_access_key_name,
+                key_vault_url=qdrant_full_access_key_secret.properties.secret_uri,
+                identity=managed_identity.identity.id,
+            ),
         ]
 
         environment_variables = [
             azure_native.app.v20230501.EnvironmentVarArgs(
-                name="AZURE-API-KEY", secret_ref=openai_key_secret_name
+                name="AZURE_OPENAI_API_KEY", secret_ref=openai_key_secret_name
             ),
             azure_native.app.v20230501.EnvironmentVarArgs(
-                name="APP-SECRET-KEY", secret_ref=app_secret_key_name
+                name="APPSECRETKEY", secret_ref=app_secret_key_name
             ),
             azure_native.app.v20230501.EnvironmentVarArgs(
-                name="WA-PASSWORD-HASH", secret_ref=wa_password_hash_name
+                name="WAPASSWORDHASH", secret_ref=wa_password_hash_name
             ),
             azure_native.app.v20230501.EnvironmentVarArgs(
-                name="ADMIN-PASSWORD-HASH", secret_ref=admin_password_hash_name
+                name="ADMINPASSWORDHASH", secret_ref=admin_password_hash_name
             ),
             azure_native.app.v20230501.EnvironmentVarArgs(
-                name="AZURE-API-URL", value=openai_url
+                name="QDRANT_WORKAROUNDS_READ_KEY",
+                secret_ref=qdrant_workarounds_read_key_name,
             ),
             azure_native.app.v20230501.EnvironmentVarArgs(
-                name="WA-USERNAME", value=wa_username
+                name="QDRANT_FULL_ACCESS_KEY",
+                secret_ref=qdrant_full_access_key_name,
             ),
             azure_native.app.v20230501.EnvironmentVarArgs(
-                name="DAILY-COST-THRESHOLD", value=daily_cost_threshold
+                name="AZURE_OPENAI_API_URL", value=openai_url
+            ),
+            azure_native.app.v20230501.EnvironmentVarArgs(
+                name="WAUSERNAME", value=wa_username
+            ),
+            azure_native.app.v20230501.EnvironmentVarArgs(
+                name="DAILYCOSTTHRESHOLD", value=daily_cost_threshold
+            ),
+            azure_native.app.v20230501.EnvironmentVarArgs(
+                name="QDRANT_URL", value=qdrant_url
+            ),
+            azure_native.app.v20230501.EnvironmentVarArgs(
+                name="AZURE_OPENAI_API_VERSION",
+                value=openai_version if openai_version else "v1",
             ),
         ]
 
@@ -188,14 +244,16 @@ class ProdDeployment:
             location=self.location,
             environment_id=container_app_environment.container_app_environment.id,
             external=True,
-            target_port=80,
+            target_port=5000,
             acr_login_server=container_registry.registry.login_server,
             acr_name=container_registry.registry.name,
             acr_username=container_registry.registry_username,  # type: ignore
             acr_password=container_registry.registry_password,  # type: ignore
-            repository=f"{self.environment}/wa-brainstormer",
+            repository=f"{self.environment}/brainstormer",
             container_name="wa-brainstormer",
             identity=managed_identity.identity,
+            host_name=HOSTNAME,
+            certificate_id=f"/subscriptions/{self.subscription_id}/resourceGroups/{self.rg_name}/providers/Microsoft.App/managedEnvironments/wa-brainstormer-container-app-environment-prod/managedCertificates/brainstormer.cwa.viadee.clou-wa-brain-250429061001",
             environment_variables=environment_variables,
             secrets=secrets,
             opts=self.opts,
